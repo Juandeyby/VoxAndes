bbc99aa1eba5802bd3c96eef6e52747024d56941
Juan Deyby Carlos-Chullo <deyby57@gmail.com>
2025-04-28 00:09:11 -0500

    feat(mummy): add mummy agent

---
+++ Assets/_App/Scripts/Enemies/Mummy/Mummy.cs
@@ -0,0 +1,117 @@
+using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mummy : MonoBehaviour
+{
+    [SerializeField] private NavMeshAgent agent;
+    public NavMeshAgent Agent => agent;
+    [SerializeField] private Animator animator;
+    public Animator Animator => animator;
+    private IMummyState _currentState;
+
+    private void Start()
+    {
+        // Initialize the Mummy's state
+        SetState(new MummyStateIdle());
+    }
+
+    public void SetState(IMummyState newState)
+    {
+        _currentState?.Exit(this);
+        _currentState = newState;
+        _currentState.Enter(this);
+    }
+    
+    private void Update()
+    {
+        _currentState?.Update(this);
+    }
+}
+
+public class MummyStateIdle : IMummyState
+{
+    public void Enter(Mummy mummy)
+    {
+        mummy.Animator.SetTrigger("Idle");
+    }
+
+    public void Update(Mummy mummy)
+    {
+        // Logic for updating idle state
+        var playerPosition = GameSingleton.Instance.PlayerManager.PlayerMovementController.transform.position;
+        var navMeshAgent = mummy.Agent;
+        
+        if (Vector3.Distance(mummy.transform.position, playerPosition) < 5f)
+        {
+            mummy.SetState(new MummyStateChase());
+        }
+    }
+
+    public void Exit(Mummy mummy)
+    {
+        // Logic for exiting idle state
+    }
+}
+
+public class MummyStateChase : IMummyState
+{
+    public void Enter(Mummy mummy)
+    {
+        mummy.Animator.SetTrigger("Walk");
+    }
+
+    public void Update(Mummy mummy)
+    {
+        // Logic for updating chase state
+        var playerPosition = GameSingleton.Instance.PlayerManager.PlayerMovementController.transform.position;
+        var navMeshAgent = mummy.Agent;
+        
+        if (Vector3.Distance(mummy.transform.position, playerPosition) <= 1f)
+        {
+            mummy.SetState(new MummyStateAttack());
+            return;
+        }
+        navMeshAgent.SetDestination(playerPosition);
+    }
+
+    public void Exit(Mummy mummy)
+    {
+        // Logic for exiting chase state
+    }
+}
+
+public class MummyStateAttack : IMummyState
+{
+    public void Enter(Mummy mummy)
+    {
+        mummy.Animator.SetTrigger("Attack");
+    }
+
+    public void Update(Mummy mummy)
+    {
+        // Logic for updating attack state
+        var playerPosition = GameSingleton.Instance.PlayerManager.PlayerMovementController.transform.position;
+        var navMeshAgent = mummy.Agent;
+
+        if (Vector3.Distance(mummy.transform.position, playerPosition) > 1f)
+        {
+            mummy.SetState(new MummyStateChase());
+            return;
+        }
+        
+        // Attack logic here
+    }
+
+    public void Exit(Mummy mummy)
+    {
+        // Logic for exiting attack state
+    }
+}
+
+public interface IMummyState
+{
+    void Enter(Mummy mummy);
+    void Update(Mummy mummy);
+    void Exit(Mummy mummy);
+}
--- Assets/_App/Scripts/Level1/Door.cs
+++ Assets/_App/Scripts/Level1/Door.cs
@@ -7,9 +7,23 @@ namespace _App.Scripts.Level1
         private static readonly int Open = Animator.StringToHash("Open");
         [SerializeField] private Animator animator;
         
-        public void OpenDoor()
+        private int _activatedCount;
+        private bool _isOpen;
+        
+        public void AddActivation()
         {
+            _activatedCount++;
+            if (_activatedCount == 2)
+            {
+                OpenDoor();
+            }
+        }
+
+        private void OpenDoor()
+        {
+            if (_isOpen) return;
             animator.SetTrigger(Open);
+            _isOpen = true;
         }
     }
 }
