20c19c0dcc406581b53c239a26121c163f5d1cac
Juan Deyby Carlos-Chullo <deyby57@gmail.com>
2025-05-04 20:10:05 -0500

    feat(traps): reorder traps

--- Assets/_App/Scripts/Traps/PlatformTrap.cs
+++ Assets/_App/Scripts/Traps/PlatformTrap.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 public class PlatformTrap : MonoBehaviour
 {
     [SerializeField] private Animator animator;
+    [SerializeField] private bool isActive = true;
     
     private void OnTriggerEnter(Collider other)
     {
+        if (!isActive) return;
         if (!other.gameObject.CompareTag("Player")) return;
         var player = other.gameObject.GetComponent<Player>();
         if (player != null)
