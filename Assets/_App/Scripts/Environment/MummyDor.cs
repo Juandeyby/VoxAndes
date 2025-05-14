6bda275a289693e09db6f122d8d0d0fa27417260
Juan Deyby Carlos-Chullo <deyby57@gmail.com>
2025-05-14 00:50:19 -0500

    feat(framents): add mummy door fragments

---
+++ Assets/_App/Scripts/Environment/MummyDor.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEngine;
+
+public class MummyDor : MonoBehaviour
+{
+    [SerializeField] private BoxCollider boxCollider;
+    [SerializeField] private GameObject[] doorFragments;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.tag);
+        if (other.CompareTag("Mummy"))
+        {
+            foreach (var fragment in doorFragments)
+            {
+                var rb = fragment.GetComponent<Rigidbody>();
+                if (rb != null)
+                {
+                    rb.AddForce(Vector3.forward * -20f, ForceMode.Impulse);
+                }
+            }
+        }
+        boxCollider.enabled = false;
+    }
+}
